65a1ec82c600cb4d202f0aa9eaf055be4dae61f5
Stefano David <stefano.david.bz@gmail.com>
2019-04-19 10:45:12 +0200

    Tentative workaround for sphinxcontrib-googleanalytics broken extension

--- source/conf.py
+++ source/conf.py
@@ -39,8 +39,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.extlinks',
     'sphinx.ext.imgconverter',
-    'sphinx_tabs.tabs',
-    'sphinxcontrib.googleanalytics'
+    'sphinx_tabs.tabs'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -467,6 +466,38 @@ epub_exclude_files = ['search.html']
 #
 # epub_use_index = True
 
+# Tentative workaround for sphinxcontrib-googleanalytics which uses an
+# old, obsolete import.
+
+from sphinx.errors import ExtensionError
+
+def add_ga_javascript(app, pagename, templatename, context, doctree):
+    if not app.config.googleanalytics_enabled:
+        return
+
+    metatags = context.get('metatags', '')
+    metatags += """<script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '%s']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>""" % app.config.googleanalytics_id
+    context['metatags'] = metatags
+
+def check_config(app):
+    if not app.config.googleanalytics_id:
+        raise ExtensionError("'googleanalytics_id' config value must be set for ga statistics to function properly.")
 
 def setup(app):
-    app.add_stylesheet('https://use.fontawesome.com/releases/v5.7.0/css/all.css')
+    app.add_stylesheet('https://use.fontawesome.com/releases/v5.7.0/css/all.css')  
+    app.add_config_value('googleanalytics_id', '', 'html')
+    app.add_config_value('googleanalytics_enabled', True, 'html')
+    app.connect('html-page-context', add_ga_javascript)
+    app.connect('builder-inited', check_config)
+    return {'version': '0.1'}
