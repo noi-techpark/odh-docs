6e2f453c455118f2433481e3fdf52d9f13688a19
Stefano David <stefano.david.bz@gmail.com>
2019-11-26 18:27:48 +0100

    remove google analitycs code #84

--- source/conf.py
+++ source/conf.py
@@ -157,8 +157,6 @@ html_show_sourcelink = False
 #
 html_show_sphinx = False
 
-googleanalytics_id = 'UA-138331709-2'
-
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'OpenDataHubDocsdoc'
 
@@ -216,39 +214,6 @@ epub_publisher = author
 epub_copyright = copyright
 epub_exclude_files = ['search.html']
 
-# Tentative workaround for sphinxcontrib-googleanalytics which uses an
-# old, obsolete import.
-
-from sphinx.errors import ExtensionError
-
-def add_ga_javascript(app, pagename, templatename, context, doctree):
-    if not app.config.googleanalytics_enabled:
-        return
-
-    metatags = context.get('metatags', '')
-    metatags += """<script type="text/javascript">
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '%s']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>""" % app.config.googleanalytics_id
-    context['metatags'] = metatags
-
-def check_config(app):
-    if not app.config.googleanalytics_id:
-        raise ExtensionError("'googleanalytics_id' config value must be set for ga statistics to function properly.")
-
 def setup(app):
     app.add_css_file('https://use.fontawesome.com/releases/v5.7.0/css/all.css')
     app.add_css_file('css/odh.css')
-    app.add_config_value('googleanalytics_id', '', 'html')
-    app.add_config_value('googleanalytics_enabled', True, 'html')
-    app.connect('html-page-context', add_ga_javascript)
-    app.connect('builder-inited', check_config)
-    return {'version': '0.1'}
