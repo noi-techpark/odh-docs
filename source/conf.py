50dce0162dc2ae7e6bbf23e7e395a5774e6a7688
Stefano David <code@stefanodavid.eu>
2022-03-18 11:52:29 +0100

    simplify exclude_pattern

--- source/conf.py
+++ source/conf.py
@@ -87,9 +87,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = [ 'replace.rst', 'dtp.rst', 'datasets',
-                     'domains.rst', 'architecture.rst',
-                     'howto/tourism/access.rst', 'includes', 'kg.rst' ] 
+exclude_patterns = [ 'replace.rst', 'datasets', 'includes' ] 
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
