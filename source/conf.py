d989f47e60891c066c9732bc09a8fb5161b38ea4
Stefano David <code@stefanodavid.eu>
2023-09-29 10:13:38 +0200

    bump version

--- source/conf.py
+++ source/conf.py
@@ -74,7 +74,7 @@ author = u'The Open Data Hub Team'
 # built documents.
 #
 # The short X.Y version.
-version = u'2023.07'
+version = u'2023.09'
 # The full version, including alpha/beta/rc tags.
 release = version
 
